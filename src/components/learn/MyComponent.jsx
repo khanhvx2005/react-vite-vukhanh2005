70ccac33e4c40722a3c01ab5881594ac4b0fe011
khanhvx2005 <kvuxuan789@gmail.com>
2025-11-22 22:10:22 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,11 +2,19 @@
 // JSX
 // Fragment
 // Sử dụng CSS
+// Sử dụng 1 biến số với JSX sử dụng cú pháp {} cho phép code JS trong HTML
 const MyComponent = () => {
+    // const hoidanit = "eric"; // string
+    // const hoidanit = 25; // number
+    // const hoidanit = true;
+    // const hoidanit = undefined;
+    // const hoidanit = null
+    // Đối với trường hợp kiểu dữ liệu là boolean , undefined , null thì việc dùng biến số sẽ vô nghĩa
+    const hoidanit = [1, 2, 3]
     return (
         <>
             <div>fullname</div>
-            <div style={{ color: "red" }}> Vu xuan khanh</div>
+            <div style={{ color: "red" }}> {JSON.stringify(hoidanit)}</div>
         </>
     );
 }
